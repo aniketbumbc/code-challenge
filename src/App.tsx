b1e12a851e08ea3f3754ffa4abd777300fdb9765
Christophe Johanny <christophe.johanny@hey.car>
2021-09-17 10:45:58 +0200

    create react app with redux and typecript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>welcome to our challenge, relax and have fun ! 🐶</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
